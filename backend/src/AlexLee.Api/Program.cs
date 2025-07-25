ceda981824dda908c741f6c888519d0641c99def
Adam Meyer <ajacbom@pm.me>
2025-07-25 11:59:46 -0400

    updated swagger route and imported json spec file

--- backend/src/AlexLee.Api/Program.cs
+++ backend/src/AlexLee.Api/Program.cs
@@ -148,7 +148,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Alex Lee API v1");
-        c.RoutePrefix = string.Empty; // Set Swagger UI at app's root
+        c.RoutePrefix = "swagger"; // Set Swagger UI at /swagger
     });
     app.UseDeveloperExceptionPage();
 }
